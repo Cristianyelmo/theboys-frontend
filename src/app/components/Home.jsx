e0c10fab944248a8720452ec40fd387d7b402e88
Cristianyelmo <cristianama2019@gmail.com>
2024-08-02 18:53:33 -0300

    test start

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -9,84 +9,48 @@ export default function HomeNew() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setMessage(true);
-      const audio2 = new Audio('/sound/metalpunch.mp3');
+      const audio2 = new Audio("/sound/metalpunch.mp3");
       audio2.play();
-      setStar('block')
+      setStar("block");
     }, 2800);
     return () => clearTimeout(timer);
   }, []);
-  const draggableRef = useRef(null);
 
-  const handleDragStart = (e) => {
-    e.dataTransfer.setData("text/plain", null); // Requerido para Firefox
-    e.dataTransfer.effectAllowed = "move";
-  };
-
-  const handleDragOver = (e) => {
-    e.preventDefault(); // Necesario para permitir soltar
-    e.dataTransfer.dropEffect = "move";
-  };
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const offsetX = e.clientX - draggableRef.current.clientWidth / 2;
-    const offsetY = e.clientY - draggableRef.current.clientHeight / 2;
-
-    draggableRef.current.style.left = `${offsetX}px`;
-    draggableRef.current.style.top = `${offsetY}px`;
-  };
-
-
-
-  
-const { 
-  handleStart,darkness,start,setStar}= HomeHook()
-
-  
+  const { handleStart, darkness, start, setStar } = HomeHook();
 
   return (
     <div>
-      { darkness &&  <div className="fixed inset-0 bg-black z-index my-element8"></div>} 
-    <section
+      {darkness && (
+        <div className="fixed inset-0 bg-black z-index my-element8"></div>
+      )}
+      <section className={`h-screen bg-cover-img  overflow-hidden relative  `}>
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[1000px] flex justify-center">
+        <Image
+  src="/imagen/homelander-2.png"
+  width={1000}
+  height={520}
+  className="absolute z-30 my-element2"
+/>
+<Image
+  src="/imagen/homelander-1.webp"
+  width={1000}
+  height={520}
+  className="absolute z-30 my-element"
+/>
+        <Image
+  src="/imagen/homelander-0.webp"
+  width={1000}
+  height={520}
+  className="  z-20"
+/>
+      
  
-      className={`h-screen bg-cover-img  overflow-hidden `}
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}
-    >
-      <div className="relative bottom-0 flex justify-center top-[14%]">
-        <Image
-          src="/imagen/homelander-2.png"
-          width={1000}
-          height={520}
-          className="absolute  z-30 my-element2 "
-        />
-         <Image
-          src="/imagen/homelander-1.webp"
-          width={1000}
-          height={520}
-          className="absolute  z-30 my-element"
-        />
-        <Image
-          src="/imagen/homelander-0.webp"
-          width={1000}
-          height={520}
-          className="absolute z-20 "
-        />
-       {/*  <Image
-          src="/imagen/homelander-4.png"
-          width={1000}
-          height={520}
-          className="absolute z-10 my-element3"
-        />
-        <Image
-          src="/imagen/homelander-3.png"
-          width={1000}
-          height={520}
-          className="absolute z-10 my-element4"
-        /> */}
-      </div>
 
-      <div className="relative flex flex-col  top-[70%] left-[42%] z-50">
+        </div>
+
+
+
+        <div className="relative flex flex-col  top-[70%] left-[42%] z-50">
         <Image
           src="/imagen/the-boys-logo.webp"
           width={200}
@@ -104,24 +68,31 @@ const {
       <div onClick={handleStart} className={`relative cursor-pointer flex flex-col  top-[90%] left-[39%] z-50 ${start} ${start  && 'my-element7'}`}>
         <p className="text-white ">Haz click para comenzar</p>
       </div>
-
-      <div
-        ref={draggableRef}
-        className="absolute top-[50%] left-[20%] z-50 cursor-move"
-        draggable="true"
-        onDragStart={handleDragStart}
-        onDragOver={handleDragOver}
-        onDrop={handleDrop}
-      >
-       {/*  <Image
-          src="/imagen/butcher-0.webp"
-          width={300}
-          height={300}
-          alt="Imagen arrastrable"
-          className=""
-        /> */}
-      </div>
-    </section>
+      </section>
     </div>
   );
 }
+
+{
+  /* <div className="relative flex flex-col  top-[70%] left-[42%] z-50">
+        <Image
+          src="/imagen/the-boys-logo.webp"
+          width={200}
+          height={200}
+          className={`absolute opacity-0 ${message && "my-element6"}`}
+        />
+        <Image
+          src="/imagen/the-boys-logo-blur.webp"
+          width={200}
+          height={200}
+          className={`absolute opacity-0 ${message && "my-element5"}`}
+        />
+      </div>
+
+      <div onClick={handleStart} className={`relative cursor-pointer flex flex-col  top-[90%] left-[39%] z-50 ${start} ${start  && 'my-element7'}`}>
+        <p className="text-white ">Haz click para comenzar</p>
+      </div> */
+}
+
+
+
